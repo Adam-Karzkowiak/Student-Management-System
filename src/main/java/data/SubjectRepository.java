24da3114c835a5be1be93701d0b5747e07750c40
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-10-31 18:10:56 +0100

    Method addToRepository renamed to addToSubjectRepository

--- src/main/java/data/SubjectRepository.java
+++ src/main/java/data/SubjectRepository.java
@@ -9,7 +9,7 @@ public class SubjectRepository {
 
 //addsubject np
 
-    public ArrayList<Subject> addToRepository(Subject subject) {
+    public ArrayList<Subject> addSubjectToRepository(Subject subject) {
         SubjectRepository.subjectDatabase.add(subject);
         return SubjectRepository.subjectDatabase;
     }
