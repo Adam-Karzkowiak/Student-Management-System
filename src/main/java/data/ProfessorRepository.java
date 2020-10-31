c1906b9b4eac215aaaeef50edb8c0d58253f0ade
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-10-31 21:50:43 +0100

    New method in ProfessorRepository - deleteProfessor.

--- src/main/java/data/ProfessorRepository.java
+++ src/main/java/data/ProfessorRepository.java
@@ -2,14 +2,25 @@ package data;
 
 
 import model.Professor;
+import model.Student;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ProfessorRepository {
     public static ArrayList<Professor> professorDatabase = new ArrayList<>();
 
     public void addToDatabase(Professor professor){
         professorDatabase.add(professor);
+    }
+
+    public void deleteProfessor(String pesel) {
+        Iterator<Professor> iterator = professorDatabase.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().getPesel().equals(pesel)) {
+                iterator.remove();
+            }
+        }
     }
 
 }
