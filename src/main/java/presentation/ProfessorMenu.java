b4d93261a6af281abbac9866de390c2a2a041f73
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-11-02 23:09:10 +0100

    Wording

--- src/main/java/presentation/ProfessorMenu.java
+++ src/main/java/presentation/ProfessorMenu.java
@@ -9,7 +9,7 @@ public class ProfessorMenu {
 
     final static Scanner scanDecision = new Scanner(System.in);
     private static int action;
-private SubjectService subjectService;
+    private SubjectService subjectService;
 
     public ProfessorMenu(SubjectService subjectService) {
         this.subjectService = subjectService;
@@ -17,7 +17,7 @@ private SubjectService subjectService;
 
     public void professorMenu() {
         do {
-            System.out.println(LoggedUser.professor.getName()+" "+LoggedUser.professor.getSurname()+" professor panel");
+            System.out.println(LoggedUser.professor.getName() + " " + LoggedUser.professor.getSurname() + " professor panel");
             System.out.println("1. Show a list of subjects");
             System.out.println("2. Add subject");
             System.out.println("3. Show registered students to a subject");
@@ -28,8 +28,8 @@ private SubjectService subjectService;
             action = scanDecision.nextInt();
             switch (action) {
                 case 1:
-                   subjectService.showSubjectList();
-                   break;
+                    subjectService.showSubjectList();
+                    break;
                 case 2:
                     System.out.println("Create subject.");
                     System.out.println("Subject title: ");
@@ -48,26 +48,29 @@ private SubjectService subjectService;
                 case 4:
                     System.out.println("Show average grades from subject");
                     System.out.println("Provide subject name :");
-                    subjectName=scanDecision.nextLine();
+                    scanDecision.nextLine();
+                    subjectName = scanDecision.nextLine();
                     subjectService.calculateAverageForWholeClass(subjectName);
                     break;
                 case 5:
                     System.out.println("Show student grades from subject");
-                    System.out.println("Student pesel");
-                    String studentPesel=scanDecision.nextLine();
+                    System.out.println("Student pesel:");
+                    scanDecision.nextLine();
+                    String studentPesel = scanDecision.nextLine();
                     System.out.println("Subject name");
-                    subjectName=scanDecision.nextLine();
-                    subjectService.calculateAvgForStudent(subjectName,studentPesel);
+                    subjectName = scanDecision.nextLine();
+                    subjectService.calculateAvgForStudent(subjectName, studentPesel);
                     break;
                 case 6:
                     System.out.println("Grading a student");
                     System.out.println("Subject name :");
-                    subjectName=scanDecision.nextLine();
+                    scanDecision.nextLine();
+                    subjectName = scanDecision.nextLine();
                     System.out.println("Student pesel number :");
-                    studentPesel=scanDecision.nextLine();
+                    studentPesel = scanDecision.nextLine();
                     System.out.println("Grade : ");
-                    int grade=scanDecision.nextInt();
-                    subjectService.giveAGrade(subjectName,studentPesel,grade);
+                    int grade = scanDecision.nextInt();
+                    subjectService.giveAGrade(subjectName, studentPesel, grade);
                     break;
             }
 
