310ae5082a6b2d572f60da0f5fd3802ec9f17e94
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-11-13 21:39:18 +0100

    Annotation @Service added.

--- src/main/java/logic/AdminService.java
+++ src/main/java/logic/AdminService.java
@@ -1,8 +1,10 @@
 package logic;
 
 import lombok.Data;
+import org.springframework.stereotype.Service;
 
 @Data
+@Service
 public class AdminService {
     final static String login = "admin";
     final static String password = "admin";
