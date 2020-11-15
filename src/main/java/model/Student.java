24da69c2d03e946be4be7b69ac0d500bfd2503c0
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-11-15 22:32:26 +0100

    @Component annotation added

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -2,8 +2,10 @@ package model;
 
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 @Data
+@Component
 public class Student extends User {
     public Student(int id, String login, String password, String name, String surname, String pesel) {
         super(id, login, password, name, surname, pesel);
