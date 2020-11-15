178dd89eb75e6124e31155bca752d3511ac063ff
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-11-15 22:33:21 +0100

    @Component annotation added

--- src/main/java/authorization/LoggedUser.java
+++ src/main/java/authorization/LoggedUser.java
@@ -2,7 +2,9 @@ package authorization;
 
 import model.Professor;
 import model.Student;
+import org.springframework.stereotype.Component;
 
+@Component
 public class LoggedUser {
     public static Professor professor=null;
     public static Student student=null;
