bb391f38ddd79f4da7a98cc0b6ba4e350328f0d8
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-11-17 22:43:43 +0100

    Annotations refactoring.

--- src/main/java/app/authorization/LoggedUser.java
+++ src/main/java/app/authorization/LoggedUser.java
@@ -2,9 +2,7 @@ package app.authorization;
 
 import app.model.Professor;
 import app.model.Student;
-import org.springframework.stereotype.Component;
 
-@Component
 public class LoggedUser {
     public static Professor professor=null;
     public static Student student=null;
