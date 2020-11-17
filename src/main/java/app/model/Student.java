15a46fac55aa4c11f71228646e276f0ac1a54632
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-11-17 22:42:11 +0100

    Annotations refactoring.

--- src/main/java/app/model/Student.java
+++ src/main/java/app/model/Student.java
@@ -2,10 +2,8 @@ package app.model;
 
 
 import lombok.Data;
-import org.springframework.stereotype.Component;
 
 @Data
-@Component
 public class Student extends User {
     public Student(int id, String login, String password, String name, String surname, String pesel) {
         super(id, login, password, name, surname, pesel);
