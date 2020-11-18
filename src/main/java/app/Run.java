aafad24a3a2c1527f3ec6e54569f3b286e69e00c
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-11-18 17:28:23 +0100

    Run in the hands of Spring (DI)

--- src/main/java/app/Run.java
+++ src/main/java/app/Run.java
@@ -1,32 +1,15 @@
 package app;
 
-import app.data.ProfessorRepository;
-import app.data.StudentRepository;
-import app.data.SubjectRepository;
-import app.logic.IdentifierProvider;
-import app.logic.ProfessorService;
-import app.logic.StudentService;
-import app.logic.SubjectService;
+import app.configuration.Config;
 import app.presentation.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-@SpringBootApplication
 public class Run {
-    @Autowired
     public static void main(String[] args) {
-        ProfessorRepository professorRepository=new ProfessorRepository();
-        StudentRepository studentRepository=new StudentRepository();
-        SubjectRepository subjectRepository=new SubjectRepository();
-        IdentifierProvider identifierProvider=new IdentifierProvider();
-        ProfessorService professorService=new ProfessorService(identifierProvider,professorRepository);
-        StudentService studentService=new StudentService(identifierProvider,studentRepository);
-        SubjectService subjectService=new SubjectService(subjectRepository);
-        AdminMenu adminMenu=new AdminMenu(professorService,studentService);
-        LoginMenu loginMenu=new LoginMenu(professorService,studentService);
-        ProfessorMenu professorMenu=new ProfessorMenu(subjectService);
-        StudentMenu studentMenu=new StudentMenu(studentService,subjectService);
-        ControllerMenu controllerMenu=new ControllerMenu(loginMenu,adminMenu,professorMenu,studentMenu);
+        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        ControllerMenu controllerMenu = context.getBean("controllerMenu",ControllerMenu.class);
         controllerMenu.callLoginMenu();
+
     }
 }
