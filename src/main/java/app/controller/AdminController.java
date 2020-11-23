413175e7a83b4a883d24e6f73973562936c2ad5f
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-11-23 20:45:09 +0100

    class fields and constructor created, added @Autowired

--- src/main/java/app/controller/AdminController.java
+++ src/main/java/app/controller/AdminController.java
@@ -1,7 +1,22 @@
 package app.controller;
 
+import app.service.ProfessorService;
+import app.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Scanner;
 
 @RestController
 public class AdminController {
+    static final Scanner scanDecision = new Scanner(System.in);
+    private static int action;
+    ProfessorService professorService;
+    StudentService studentService;
+
+    @Autowired
+    public AdminController(ProfessorService professorService, StudentService studentService) {
+        this.professorService = professorService;
+        this.studentService = studentService;
+    }
 }
