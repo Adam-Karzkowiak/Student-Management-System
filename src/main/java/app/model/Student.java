b24cfcd16db434d49632f7952054f884d824eccc
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-11-29 17:01:24 +0100

    added lombok annotations

--- src/main/java/app/model/Student.java
+++ src/main/java/app/model/Student.java
@@ -1,11 +1,12 @@
 package app.model;
 
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.*;
 
-@EqualsAndHashCode(callSuper = true)
-@Data
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Student extends User {
     public Student(int id, String login, String password, String name, String surname, String pesel) {
         super(id, login, password, name, surname, pesel);
