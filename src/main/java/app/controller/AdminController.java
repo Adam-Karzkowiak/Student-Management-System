8f8ce719fea93c591c866226a306c8b4fa2c9fa8
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-11-29 16:35:10 +0100

    Delete professor method in AdminController refactored

--- src/main/java/app/controller/AdminController.java
+++ src/main/java/app/controller/AdminController.java
@@ -1,6 +1,8 @@
 package app.controller;
 
 
+import app.data.ProfessorRepository;
+import app.exception.ProfessorNotFoundException;
 import app.model.Professor;
 import app.model.Student;
 import app.service.ProfessorService;
@@ -8,6 +10,7 @@ import app.service.StudentService;
 import app.service.ValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -47,23 +50,17 @@ public class AdminController {
                 newStudent.getPesel());
     }
 
-    public void callRemoveProfessorAccount() {
-        System.out.println("Provide PESEL number to delete account");
-        scanDecision.nextLine();
-        String peselToDelete = scanDecision.nextLine();
-        System.out.println("Do you want to delete ");
-        professorService.printNameAndSurname(peselToDelete);
-        System.out.println("Y/N?");
-        String decision = scanDecision.nextLine();
-        if (decision.equalsIgnoreCase("Y")) {
-            professorService.removeProfessorAccount(peselToDelete);
-            System.out.println("Account has been deleted");
-        } else {
-            System.out.println("Procedure has been cancelled");
+    @DeleteMapping("/delete/professors")
+    public ResponseEntity<String> callRemoveProfessorAccount(@RequestBody String pesel) {
+        if (ProfessorRepository.professorDatabase.stream().noneMatch(o -> o.getPesel().equals(pesel))) {
+            throw new ProfessorNotFoundException(pesel);
         }
+        professorService.removeProfessorAccount(pesel);
+        return new ResponseEntity<>(pesel, HttpStatus.OK);
+
     }
 
-    public void callRemoveStudentAccount() {
+    public ResponseEntity<V> callRemoveStudentAccount() {
         System.out.println("Provide PESEL number to delete account");
         scanDecision.nextLine();
         String peselToDelete = scanDecision.nextLine();
