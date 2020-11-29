de1ea0b7683b90f1bdbb17a03fe8f25432976e5d
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-11-29 16:07:15 +0100

    ProfessorNotFoundException added

---
+++ src/main/java/app/exception/ProfessorNotFoundException.java
@@ -0,0 +1,11 @@
+package app.exception;
+
+
+public class ProfessorNotFoundException extends RuntimeException {
+
+    public ProfessorNotFoundException(String professorPesel) {
+        super("Could not find professor with pesel " + professorPesel);
+    }
+}
+
+}
