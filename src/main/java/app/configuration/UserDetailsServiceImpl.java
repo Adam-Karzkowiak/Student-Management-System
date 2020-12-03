681e485e6ba5e07bd7436a02cea28050fb263971
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-12-03 23:38:40 +0100

    loadUserByUsername refactored

--- src/main/java/app/configuration/UserDetailsServiceImpl.java
+++ src/main/java/app/configuration/UserDetailsServiceImpl.java
@@ -2,12 +2,16 @@ package app.configuration;
 
 import app.data.ProfessorRepository;
 import app.data.StudentRepository;
+import app.model.Professor;
+import app.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -22,7 +26,18 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String pesel) throws UsernameNotFoundException {
-        UserDetails professor = User.; //metoda retrieve student/professor w repozytoriach
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        for (Professor obj : ProfessorRepository.professorDatabase) {
+            if (obj.getLogin().equals(username)) {
+                return new User(obj.getLogin(), obj.getPassword(), Collections.emptyList());
+            }
+        }
+        for (Student obj : StudentRepository.studentDatabase) {
+            if (obj.getLogin().equals(username)) {
+                return new User(obj.getLogin(), obj.getPassword(), Collections.emptyList());
+            }
+        }
+        throw new UsernameNotFoundException(username);
     }
 }
