7efae56bdbff11f717f43f177d8216528f69fa90
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-12-07 22:20:41 +0100

    BCryptPasswordEncoder added

--- src/main/java/app/Run.java
+++ src/main/java/app/Run.java
@@ -2,12 +2,20 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @SpringBootApplication
 public class Run {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(Run.class,args);
+        SpringApplication.run(Run.class, args);
 
     }
 }
