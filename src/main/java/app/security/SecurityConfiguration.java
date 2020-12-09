f7e6cb882f7c0c800fb412737203523bcd1cf27e
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-12-09 18:11:11 +0100

    Autowired annotation added

--- src/main/java/app/security/SecurityConfiguration.java
+++ src/main/java/app/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package app.security;
 
 import app.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,6 +18,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private UserDetailsServiceImpl userDetailsService;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Autowired
     public SecurityConfiguration(UserDetailsServiceImpl userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userDetailsService = userDetailsService;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
