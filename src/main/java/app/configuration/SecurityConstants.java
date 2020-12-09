99b4b92edc5d559b8e69b99df161ad6707f06908
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-12-09 15:54:06 +0100

    Unused static field removed. Token expiration time changed to 15 min

--- src/main/java/app/configuration/SecurityConstants.java
+++ src/main/java/app/configuration/SecurityConstants.java
@@ -3,9 +3,8 @@ package app.configuration;
 
 public class SecurityConstants {
     public static final String SECRET = "SecretKeyToGenJWTs";
-    public static final long EXPIRATION_TIME = 864_000_000; // 10 days
+    public static final long EXPIRATION_TIME = 9_000_000; // 15 minut
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final String SIGN_UP_URL = "/users/sign-up";
 }
 
