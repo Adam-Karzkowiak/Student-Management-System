ac85ac088db97b04c3961d058c46358801afb748
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-12-10 00:06:55 +0100

    httpsecurity config refactored

--- src/main/java/app/security/SecurityConfiguration.java
+++ src/main/java/app/security/SecurityConfiguration.java
@@ -4,9 +4,11 @@ import app.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
@@ -27,17 +29,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().and().authorizeRequests()
+        http.cors().and().csrf().disable().authorizeRequests() //Cross-origin resource sharing-współdzielenie zasobów przez serwery na różnych domenach i Cross-site request forgery (atak-spreparowane żądania)
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/professor/**").hasRole("PROFESSOR")
                 .antMatchers("/student/**").hasRole("STUDENT")
                 .anyRequest().authenticated()
                 .and()
+                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .formLogin()
                 .permitAll()
                 .and()
-                .logout().permitAll()
-                .and()
-                .csrf().disable();
+                .logout().permitAll();
     }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+
 }
