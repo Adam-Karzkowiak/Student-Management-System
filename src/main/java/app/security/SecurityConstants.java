c9b3f24f9dbaf1f15a146cf3920fc461181411f5
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-12-09 22:46:08 +0100

    Secret key to generate JWT changed

--- src/main/java/app/security/SecurityConstants.java
+++ src/main/java/app/security/SecurityConstants.java
@@ -2,7 +2,7 @@ package app.security;
 
 
 public class SecurityConstants {
-    public static final String SECRET = "SecretKeyToGenJWTs";
+    public static final String SECRET = "IDontKnowMaybeAdamIsAnAmazingProgrammer";
     public static final long EXPIRATION_TIME = 9_000_000; // 15 minut
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
