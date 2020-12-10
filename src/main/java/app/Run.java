99bdc7e1ffabfc67a80998bc0c96b3f422ed4e01
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-12-11 00:20:08 +0100

    Spring data source auto configuration disabled

--- src/main/java/app/Run.java
+++ src/main/java/app/Run.java
@@ -2,11 +2,12 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Run {
 
     @Bean
