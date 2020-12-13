7166e2dab651fd04ec466dc9fa1245e62b809bf3
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-12-13 21:25:24 +0100

    UserDetailsService for AppUser created

---
+++ src/main/java/app/appUserDemo/service/UserDetailsServiceImplDemo.java
@@ -0,0 +1,51 @@
+package app.appUserDemo.service;
+
+import app.appUserDemo.model.AppUser;
+import app.appUserDemo.repository.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Service
+public class UserDetailsServiceImplDemo implements UserDetailsService {
+    AppUserRepository appUserRepository;
+
+
+    @Autowired
+    public UserDetailsServiceImplDemo(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        String admin = "admin";
+        AppUser appUser = appUserRepository.findByUsername(username);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        if (appUser.getUsername().equalsIgnoreCase(admin)) {
+            authorities.add(new SimpleGrantedAuthority("ADMIN"));
+            return new User(admin, admin, authorities);
+        }
+
+        if (appUser.isProfessor()) {
+            authorities.add(new SimpleGrantedAuthority("PROFESSOR"));
+            return new User(appUser.getUsername(), appUser.getPassword(), authorities);
+        }
+        if (!appUser.isProfessor()) {
+            authorities.add(new SimpleGrantedAuthority("STUDENT"));
+            return new User(appUser.getUsername(), appUser.getPassword(), authorities);
+        }
+        throw new UsernameNotFoundException(username);
+    }
+
+}
+
+
+
