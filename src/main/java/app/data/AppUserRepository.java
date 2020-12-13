8a08207f5986cced4aa4d9252094954100669214
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-12-13 20:34:09 +0100

    refactoring

--- src/main/java/app/data/AppUserRepository.java
+++ src/main/java/app/data/AppUserRepository.java
@@ -3,6 +3,8 @@ package app.data;
 import app.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     AppUser findByUsername(String username);
+
 }
