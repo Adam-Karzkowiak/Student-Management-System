45ff6f90e59db394fea4f3f828c100d799f933f3
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-12-15 23:10:53 +0100

    Demo appUser controller with constructor and sign up procedure added

--- src/main/java/app/appUserDemo/controller/AppUserController.java
+++ src/main/java/app/appUserDemo/controller/AppUserController.java
@@ -1,4 +1,29 @@
 package app.appUserDemo.controller;
 
+import app.appUserDemo.model.AppUser;
+import app.appUserDemo.repository.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AppUserController {
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private AppUserRepository appUserRepository;
+
+    @Autowired
+    public AppUserController(BCryptPasswordEncoder bCryptPasswordEncoder, AppUserRepository appUserRepository) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.appUserRepository = appUserRepository;
+    }
+
+
+    @PostMapping("/sign-up")
+    public void signUp(@RequestBody AppUser user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        appUserRepository.save(user);
+    }
 }
