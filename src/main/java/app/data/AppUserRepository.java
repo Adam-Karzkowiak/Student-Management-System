f3eed41f0e0ac25f5b8106e3354a26c1c048b8d1
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-12-28 22:01:43 +0100

    Repository annotation added.

--- src/main/java/app/data/AppUserRepository.java
+++ src/main/java/app/data/AppUserRepository.java
@@ -2,9 +2,10 @@ package app.data;
 
 import app.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
-
+@Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     AppUser findByUsername(String username);
 
