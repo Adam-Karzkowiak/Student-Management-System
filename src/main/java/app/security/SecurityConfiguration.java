e3bb4f601cea3f8825854183c7b2b5e80a94f14c
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-12-28 21:33:10 +0100

    UserDetailsServiceImpl replaced with UserDetailsServiceImplDemo

--- src/main/java/app/security/SecurityConfiguration.java
+++ src/main/java/app/security/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package app.security;
 
-import app.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
--- src/main/java/app/service/UserDetailsServiceImpl.java
+++ src/main/java/app/service/UserDetailsServiceImpl.java
@@ -1,6 +1,9 @@
 package app.service;
 
+import app.model.AppUser;
+import app.data.AppUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -14,39 +17,42 @@ import java.util.Collection;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+    AppUserRepository appUserRepository;
+    @Value("${spring.security.user.name}")
+    private String adminUserName;
 
-    StudentRepository studentRepository;
-    ProfessorRepository professorRepository;
+    @Value("${spring.security.user.password}")
+    private String adminPassword;
+
+    @Value("${spring.security.user.roles}")
+    private String adminRole;
+
 
     @Autowired
-    public UserDetailsServiceImpl(StudentRepository studentRepository, ProfessorRepository professorRepository) {
-        this.studentRepository = studentRepository;
-        this.professorRepository = professorRepository;
+    public UserDetailsServiceImpl(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        String admin = "admin";
+        AppUser appUser = appUserRepository.findByUsername(username);
         Collection<GrantedAuthority> authorities = new ArrayList<>();
-        for (Professor obj : professorRepository.professorDatabase) {
-            if (obj.getLogin().equals(username)) {
-                authorities.add(new SimpleGrantedAuthority("PROFESSOR"));
-                return new User(obj.getLogin(), obj.getPassword(), authorities);
-            }
+        if (username.equals("admin")) {
+            return User.builder().username(adminUserName).password(adminPassword).roles(adminRole).build();
         }
-        for (Student obj : studentRepository.studentDatabase) {
-            if (obj.getLogin().equals(username)) {
-                authorities.add(new SimpleGrantedAuthority("STUDENT"));
-                return new User(obj.getLogin(), obj.getPassword(), authorities);
-            }
+
+        if (appUser.isProfessor()) {
+            authorities.add(new SimpleGrantedAuthority("PROFESSOR"));
+            return new User(appUser.getUsername(), appUser.getPassword(), authorities);
         }
-        if (username.equals(admin)) {
-            authorities.add(new SimpleGrantedAuthority("ADMIN"));
-            return new User(admin, admin, authorities);
+        if (!appUser.isProfessor()) {
+            authorities.add(new SimpleGrantedAuthority("STUDENT"));
+            return new User(appUser.getUsername(), appUser.getPassword(), authorities);
         }
         throw new UsernameNotFoundException(username);
     }
 
 }
+
 
 
