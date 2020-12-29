62f55a7f3630bcfab970cf9e9394a9835d2decdc
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-12-29 21:58:31 +0100

    Overrided methods added

--- src/main/java/app/data/AppUserRepositoryImpl.java
+++ src/main/java/app/data/AppUserRepositoryImpl.java
@@ -1,7 +1,24 @@
 package app.data;
 
+import app.model.AppUser;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class AppUserRepositoryImpl implements AppUserRepository {
+
+
+    @Override
+    public AppUser findByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public AppUser save(AppUser appUser) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(long id) {
+
+    }
 }
