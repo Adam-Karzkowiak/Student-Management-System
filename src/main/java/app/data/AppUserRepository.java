f3fd1ff3f59e774b5bf7af6e076316c04c2626e8
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-12-29 21:51:01 +0100

    deleteById added

--- src/main/java/app/data/AppUserRepository.java
+++ src/main/java/app/data/AppUserRepository.java
@@ -8,5 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     AppUser findByUsername(String username);
+    void deleteById(long id);
 
 }
