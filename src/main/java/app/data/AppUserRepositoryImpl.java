a6f01ab98de6a95020e1a8cb8a98dfaf4cfa3221
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2020-12-30 22:13:35 +0100

    static appUser list added

--- src/main/java/app/data/AppUserRepositoryImpl.java
+++ src/main/java/app/data/AppUserRepositoryImpl.java
@@ -3,14 +3,24 @@ package app.data;
 import app.model.AppUser;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class AppUserRepositoryImpl implements AppUserRepository {
+    public static List<AppUser> appUserDatabase = new ArrayList<>();
 
 
     @Override
     public AppUser findByUsername(String username) {
+        for (AppUser appUser :) {
+            if (appUser.getUsername().equals(username)) {
+                return appUser;
+            }
+        }
         return null;
     }
+
 
     @Override
     public AppUser save(AppUser appUser) {
