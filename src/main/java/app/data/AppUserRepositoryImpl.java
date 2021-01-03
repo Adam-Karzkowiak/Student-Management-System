479f97be52f30145f0b99e4780f5eca204957b57
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-03 22:19:17 +0100

    Qualifier added

--- src/main/java/app/data/AppUserRepositoryImpl.java
+++ src/main/java/app/data/AppUserRepositoryImpl.java
@@ -1,11 +1,13 @@
 package app.data;
 
 import app.model.AppUser;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Qualifier("appUsers")
 @Repository
 public class AppUserRepositoryImpl implements AppUserRepository {
     public static List<AppUser> appUserDatabase = new ArrayList<>();
