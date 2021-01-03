44bee19ef63c5347e4ecc3a458a5ba7c5b818c94
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-03 20:00:45 +0100

    Added Repository annotation

--- src/main/java/app/data/SubjectRepository.java
+++ src/main/java/app/data/SubjectRepository.java
@@ -3,10 +3,12 @@ package app.data;
 
 import app.model.AppUser;
 import app.model.Subject;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class SubjectRepository {
     public static List<Subject> subjectDatabaseDemo = new ArrayList<>();
 
