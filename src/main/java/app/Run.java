182abe3a2618deb3b49e0beee23eb22e077e6b3b
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-03 22:06:03 +0100

    Run refactored

--- src/main/java/app/Run.java
+++ src/main/java/app/Run.java
@@ -2,12 +2,11 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class Run {
 
     @Bean
