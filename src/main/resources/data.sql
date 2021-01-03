28d6b4003a57de0b85d64d482e9c2938f4386c86
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-03 21:12:38 +0100

    schema.sql deleted, data.sql refactored

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,14 @@
-insert into appuser(id, username, password, name, surname, pesel, isProfessor)
-values (1, 'Adam123', 'TestHaslo123', 'Adam', 'Adamski', '90020775794', true);
+DROP TABLE IF EXISTS APPUSER;
+
+CREATE TABLE APPUSER (
+                         id INT AUTO_INCREMENT PRIMARY KEY,
+                         username VARCHAR(50) NOT NULL,
+                         password VARCHAR(100) NOT NULL,
+                         name VARCHAR(50) NOT NULL,
+                         surname VARCHAR(80) NOT NULL,
+                         pesel VARCHAR(11) NOT NULL,
+                         isProfessor BOOLEAN NOT NULL
+);
+
+INSERT INTO appuser(id, username, password, name, surname, pesel, isProfessor)
+VALUES (1, 'Adam123', 'TestHaslo123', 'Adam', 'Adamski', '90020775794', true);
--- src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE APPUSER (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(100) NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    surname VARCHAR(80) NOT NULL,
-    pesel VARCHAR(11) NOT NULL,
-    isProfessor BOOLEAN NOT NULL
-);
