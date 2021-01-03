2e55a9570226d3b003f9f36f5e7295a9df3cf1e0
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-03 20:50:29 +0100

    test data provided

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
-insert into appuser(id, username, password, name, surname, pesel, isProfessor) values (1,)
+insert into appuser(id, username, password, name, surname, pesel, isProfessor)
+values (1, 'Adam123', 'TestHaslo123', 'Adam', 'Adamski', '90020775794', true);
