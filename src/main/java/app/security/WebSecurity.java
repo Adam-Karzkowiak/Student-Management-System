092e76a4fb8c0f324afea23a781994565db1a94f
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-10 18:38:30 +0100

    Unused annotations deleted

--- src/main/java/app/security/WebSecurity.java
+++ src/main/java/app/security/WebSecurity.java
@@ -1,10 +1,8 @@
 package app.security;
 
 import app.service.UserDetailsServiceImpl;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,8 +14,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
-@Configuration
-@RequiredArgsConstructor
+
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
