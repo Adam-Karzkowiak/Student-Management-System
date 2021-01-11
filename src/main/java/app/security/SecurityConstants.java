4881b0ff93f5e77d0c82ae99b409a9f71ab18bd1
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-11 23:33:46 +0100

    token expiration time changed

--- src/main/java/app/security/SecurityConstants.java
+++ src/main/java/app/security/SecurityConstants.java
@@ -3,7 +3,7 @@ package app.security;
 
 public class SecurityConstants {
     public static final String SECRET = "SecretKeyToGenJWTs";
-    public static final long EXPIRATION_TIME = 864_000_000;
+    public static final long EXPIRATION_TIME = 64_000_000;
     public static final String TOKEN_PREFIX = "Bearer";
     public static final String HEADER_STRING = "Authorization";
 }
