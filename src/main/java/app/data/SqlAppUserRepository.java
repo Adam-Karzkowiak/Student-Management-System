2733c2cac15733cf65da9a4c85970051bc72f312
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-29 21:35:11 +0100

    SqlAppUserRepository annotated as a Repository, extends:JpaRepository and AppUserRepository

--- src/main/java/app/data/SqlAppUserRepository.java
+++ src/main/java/app/data/SqlAppUserRepository.java
@@ -1,5 +1,12 @@
 package app.data;
 
 
-class SqlAppUserRepository {
+import app.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface SqlAppUserRepository extends JpaRepository<AppUser, Integer>, AppUserRepository {
+    @Override
+    AppUser findByUsername(String username);
 }
