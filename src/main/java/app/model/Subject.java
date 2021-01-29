a1e5fb61e2d4d12c06b12b96c9daf8a9929473c6
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-29 21:58:40 +0100

    AllArgsConstructor added

--- src/main/java/app/model/Subject.java
+++ src/main/java/app/model/Subject.java
@@ -1,5 +1,6 @@
 package app.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.util.Map;
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Subject {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
