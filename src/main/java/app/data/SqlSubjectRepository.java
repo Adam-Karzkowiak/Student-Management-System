c15dc5f0d61312e7ce5ad1aa65f0f23ce8b556b9
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-30 18:38:41 +0100

    SqlSubjectRepository extends SubjectRepository interface

--- src/main/java/app/data/SqlSubjectRepository.java
+++ src/main/java/app/data/SqlSubjectRepository.java
@@ -1,9 +1,24 @@
 package app.data;
 
+import app.model.AppUser;
 import app.model.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
-interface SqlSubjectRepository extends JpaRepository<Subject, Integer> {
+interface SqlSubjectRepository extends JpaRepository<Subject, Integer>, SubjectRepository {
+
+
+    @Override
+    void addStudentToSubject(Subject subject, AppUser appUser) {
+        if (!subject.grades.containsKey(appUser)) {
+            subject.grades.put(appUser, new ArrayList<>());
+        }
+    }
+
+    public void addAGrade(Subject subject, AppUser appUser, int grade) {
+        subject.grades.get(appUser).add(grade);
+    }
 }
