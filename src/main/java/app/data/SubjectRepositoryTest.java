325a8edbe2dc56652033c24f5bbe17c2f3968ade
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-01-30 18:35:31 +0100

    SubjectRepository methods moved to interface

--- src/main/java/app/data/SubjectRepositoryTest.java
+++ src/main/java/app/data/SubjectRepositoryTest.java
@@ -1,4 +1,18 @@
 package app.data;
 
+import app.model.AppUser;
+import app.model.Subject;
+
+
 public interface SubjectRepositoryTest {
+
+    Subject save(Subject subject);
+
+    void deleteById(Integer id);
+
+    void addStudentToSubject(Subject subject, AppUser appUser);
+
+    void giveAGrade(Subject subject, AppUser appUser, int grade);
+
+
 }
