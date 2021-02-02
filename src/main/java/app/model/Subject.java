818f84eee1f45108564d98c4464be434157838fb
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-02 22:09:15 +0100

    grades Map annotated with ElementCollection

--- src/main/java/app/model/Subject.java
+++ src/main/java/app/model/Subject.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,6 +17,8 @@ public class Subject {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String subjectName;
+
+    @ElementCollection
     public Map<AppUser, ArrayList<Integer>> grades;
 
 
